f3b5f43744b2e071fb59fd3c155ef7c7eee75a91
XyLnEn <le_pingouin_dbil@hotmail.fr>
2015-11-25 19:17:53 +0100

    implemented FlightplanImplTest

--- Test_Integration_2/src/fr/nantes/cta/test/FlightPlanImplTest.java
+++ Test_Integration_2/src/fr/nantes/cta/test/FlightPlanImplTest.java
@@ -2,13 +2,18 @@ package fr.nantes.cta.test;
 
 import static org.junit.Assert.*;
 
+import java.util.List;
+import java.util.Vector;
+
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
 import fr.nantes.cta.Airway;
 import fr.nantes.cta.CompassDirection;
+import fr.nantes.cta.TakenAirway;
 import fr.nantes.cta.one.AirwayImpl;
+import fr.nantes.cta.one.FlightPlanImpl;
 import fr.nantes.cta.one.LatitudeImpl;
 import fr.nantes.cta.one.LongitudeImpl;
 import fr.nantes.cta.one.PositionImpl;
@@ -17,10 +22,9 @@ import fr.nantes.cta.one.VORImpl;
 
 public class FlightPlanImplTest {
 
-	private TakenAirwayImpl instance;
+	private TakenAirwayImpl tair1,tair2,tair3,tair4,tair5,tair6;
     private Airway air1,air2,air3,air4,air5,air6;
-    private int alt1;
-    private CompassDirection cd1;
+    private CompassDirection cd1,cd2;
 	
 	@Before
 	public void setUp() throws Exception {
@@ -33,9 +37,17 @@ public class FlightPlanImplTest {
 		air2 = new AirwayImpl(o,r);
 		air3 = new AirwayImpl(r,v);
 		air4 = new AirwayImpl(r,i);
-		air5 = new AirwayImpl(v,o);
+		air5 = new AirwayImpl(o,v);
 		air6 = new AirwayImpl(v,v);
 		cd1 = CompassDirection.NORTH;
+		cd2 = CompassDirection.SOUTH;
+		
+		tair1 = new TakenAirwayImpl(air1, 1000, cd1);
+		tair2 = new TakenAirwayImpl(air2, 1500, cd1);
+		tair3 = new TakenAirwayImpl(air3, 1200, cd1);
+		tair4 = new TakenAirwayImpl(air4, 3000, cd1);
+		tair5 = new TakenAirwayImpl(air5, 1000, cd2);
+		tair6 = new TakenAirwayImpl(air6, 0, cd1);
 	}
 
 	@After
@@ -44,22 +56,40 @@ public class FlightPlanImplTest {
 
 	@Test
 	public void testFlightPlanImpl() {
-		fail("Not yet implemented");
+		FlightPlanImpl f = new FlightPlanImpl();
+		assertNotNull(f);
 	}
 
 	@Test
 	public void testAddAirway() {
-		fail("Not yet implemented");
+		FlightPlanImpl f = new FlightPlanImpl();
+		f.addAirway(tair1);
+		assertSame(tair1, f.getPath().get(0));
 	}
 
 	@Test
 	public void testDistance() {
-		fail("Not yet implemented");
+		FlightPlanImpl f = new FlightPlanImpl();
+		f.addAirway(tair1);
+		double dist1 = f.distance();
+		assert dist1 >0;
+		f.addAirway(tair5);
+		assertEquals(dist1*2, f.distance(), 0);
+		
+		
 	}
 
 	@Test
 	public void testGetPath() {
-		fail("Not yet implemented");
+		FlightPlanImpl f = new FlightPlanImpl();
+		f.addAirway(tair1);
+		f.addAirway(tair2);
+		f.addAirway(tair3);
+		List<TakenAirway> list = new Vector<TakenAirway>();
+		list.add(tair1);
+		list.add(tair2);
+		list.add(tair3);
+		assertEquals(list, f.getPath());
 	}
 
 }
